8f6f52c4e375a858e55a0a244c89605447d61675
Ali Günay <57919680+AliGunaay@users.noreply.github.com>
2023-08-30 06:27:55 +0300

    Add files via upload

---
+++ Repository/IPostRepository.cs
@@ -0,0 +1,16 @@
+using back_side_Model.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace back_side_DataAccess.Repositories
+{
+    public interface IPostRepository
+    {
+        Task<List<Post>> GetAllPosts();
+        Task<List<Post>> GetPostsByEmail(string email);
+        Task<Post> GetPostById(int postId);
+        Task CreatePost(Post post);
+        Task UpdatePost(Post post);
+        Task DeletePost(int postId);
+    }
+}
